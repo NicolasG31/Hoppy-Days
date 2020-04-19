7ba80a5d5bf36916673e2f926492338d4a3284e6
Nicolas <nicolas.guillon@epitech.eu>
2020-04-19 12:49:40 +0200

    _on_ceiling, parallax, game over, spikes

---
+++ Scenes/Hazards/Hazards.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+
+
+func _on_SpikeTop_body_entered(body):
+	if body.has_method("hurt"):
+		body.hurt()
+	
--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -6,6 +6,9 @@ const SPEED = 1000
 const GRAVITY = 100
 const jUMP_SPEED = 2000
 const UP = Vector2(0, -1)
+const WORLD_LIMIT = 3000
+
+var lives = 3
 
 signal animate
 
@@ -17,8 +20,12 @@ func _physics_process(delta):
 	move_and_slide(motion, UP)
 
 func apply_gravity():
+	if position.y > WORLD_LIMIT:
+		end_game()
 	if is_on_floor():
 		# Applying 5 because there is a bug in is_on_floor()
+		motion.y = 5
+	elif is_on_ceiling():
 		motion.y = 5
 	else:
 		motion.y += GRAVITY
@@ -38,3 +45,13 @@ func move():
 func animate():
 	emit_signal("animate", motion)
 	
+func end_game():
+	get_tree().change_scene("res://Scenes/GameOver.tscn")
+	
+func hurt():
+	position.y -= 1
+	yield(get_tree(), "idle_frame")
+	motion.y -= jUMP_SPEED
+	lives -= 1
+	if lives == 0:
+		end_game()
